d6840b1ed477d37591f38d716297218067ce0b63
Gustavo Henrique <gustavohenriquelive12@gmail.com>
2024-01-10 15:02:23 -0300

    Layout primario

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,15 @@
 import { Routes } from '@angular/router';
 import { AbrirChamadoComponent } from './components/pages/abrir-chamado/abrir-chamado.component';
 import { HomeComponent } from './components/pages/home/home.component';
+import { AdmComponent } from './components/pages/adm/adm.component';
 
 export const routes: Routes = [
 
     {path: 'home', component:HomeComponent},
 
     {path: 'abrir-chamado', component:AbrirChamadoComponent},
+
+    {path: 'adm', component:AdmComponent},
 
     {path: '**', redirectTo: 'home'}
 
