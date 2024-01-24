0195a4c502f6e331633f73ffd99c02986b1e6a74
Gustavo Henrique <gustavohenriquelive12@gmail.com>
2024-01-24 12:23:28 -0300

    .

--- src/app/components/pages/abrir-chamado/abrir-chamado.component.ts
+++ src/app/components/pages/abrir-chamado/abrir-chamado.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-abrir-chamado',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './abrir-chamado.component.html',
   styleUrl: './abrir-chamado.component.css'
 })
 export class AbrirChamadoComponent {
+
+  servidor = new FormControl
 
 }
